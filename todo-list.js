ab1e24dd86cf12209b6f7a43aeeba6c55c39088e
Diogo Teixeira <diogoantonio_77@hotmail.com>
2024-10-29 23:55:17 +0100

    Event Listeners

--- todo-list.js
+++ todo-list.js
@@ -3,8 +3,6 @@ const todoList = [];
     function renderTodo() {
         
         let todoListHtml = '';
-
-
         todoList.forEach((todoObject,index) => {
             const name = todoObject.name;
             const dueDate = todoObject.date;
@@ -12,18 +10,27 @@ const todoList = [];
             const html = `
             <div>${name}</div>
             <div>${dueDate}</div>
-            <button onClick="
-                todoList.splice(${index},1);
-                renderTodo();
-                console.log(todoList);
-            " class="delete-todo-button">
+            <button class="delete-todo-button js-delete-todo-button">
             Delete</button>
             `;
             todoListHtml += html;
-        })
+        });
         document.querySelector('.js-todo-list').innerHTML = todoListHtml;
-    }
-        
+
+        //because we have several delete buttons we have to use the forEach, and the querySelectorAll
+        document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton,index) => {
+            deleteButton.addEventListener('click',() => {
+                todoList.splice(index, 1);
+                renderTodo();
+                console.log(todoList);
+            });
+        });
+    };
+    
+    document.querySelector('.js-add-todo-button').addEventListener('click',() =>{
+        addTodo();
+    });
+
     function addTodo() {
         const inputElement = document.querySelector('.js-name-input');
         const dateInputElement = document.querySelector('.js-date-input');
@@ -42,4 +49,4 @@ const todoList = [];
         inputElement.value = ''; //this will clear the text box
 
         renderTodo();
-    }
+    };
